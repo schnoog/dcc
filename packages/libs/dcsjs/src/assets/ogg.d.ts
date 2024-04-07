46d3e7c5a94b6afbb6401f56ecc5bf929d90ed4f
Andreas Lackinger <andreas.lackinger@unkreativ.at>
2024-04-07 14:38:44 +0200

    build: fix tsconfigs for dcsjs merge

--- packages/libs/dcsjs/src/assets/ogg.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "*.ogg" {
-	const value: string;
-	export default value;
-}
---
+++ utils/config/fileTypes.d.ts
@@ -0,0 +1,24 @@
+declare module "*.module.less" {
+	const resource: { [key: string]: string };
+	export default resource;
+}
+
+declare module "*.lua" {
+	const value: string;
+	export default value;
+}
+
+declare module "*.ogg" {
+	const value: string;
+	export default value;
+}
+
+declare module "*.png" {
+	const value: string;
+	export default value;
+}
+
+declare module "*.template" {
+	const value: string;
+	export default value;
+}
