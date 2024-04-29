a213dcfff22ff043cf2c7a15b93b8b2bcb3710af
Jürgen Dressel <github.com.copper397@passinbox.com>
2024-04-29 07:53:29 +0200

    feat(web): hybrid

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -8,7 +8,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), react()],
-	output: "server",
+	output: "hybrid",
 	adapter: cloudflare({
 		mode: "directory",
 	}),
