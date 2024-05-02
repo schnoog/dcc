3f24cf7da9073c33be385418ae50f5133f0a77e7
Jürgen Dressel <github.com.copper397@passinbox.com>
2024-05-02 18:02:32 +0200

    fix(web): astro compress

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://web.digitalcrewchief.at/",
@@ -13,6 +15,13 @@ export default defineConfig({
 		}),
 		react(),
 		sitemap(),
+		compress({
+			css: true,
+			html: true,
+			img: false,
+			js: true,
+			svg: true,
+		}),
 	],
 	output: "hybrid",
 	adapter: cloudflare({
